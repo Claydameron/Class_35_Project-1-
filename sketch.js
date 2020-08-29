8e182847496c639b4e4be55db31d109ba7badf50
Claydameron <66542550+Claydameron@users.noreply.github.com>
2020-08-29 17:05:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 //edited
 var dog, dogImg, happyDogImg, database, foodS, foodStock;
 var feedButton, addButton;
+var foodObj, fedTime, lastFed;
 
 function preload()
 {
   dogImg = loadImage("images/dogImg.png");
   happyDogImg = loadImage("images/dogImg1.png");
-  
 }
 
 function setup() {
@@ -26,6 +26,12 @@ function setup() {
 
   addButton = createButton("Add food");
   addButton.position(675,65);
+
+  foodObj = new Food(200,200);
+
+  feedButton.mousePressed(feedDog);
+  addButton.mousePressed(addFood);
+
   
   
 }
@@ -51,33 +57,52 @@ textSize(13);
 text("Note: Press the UP ARROW Key To Feed Drago Milk!",115,10,300,20);
 */
 
-
-
-
+foodObj.display();
   
 
   drawSprites();
+
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed >= 12) {
+    text("Last fed at: " + lastFed % 12 + "PM", 50,30);
+  } else if(lastFed == 0) {
+    text("Last fed at: 12 AM",50,30);
+  } else{
+    text("Last fed at: " + lastFed + "AM", 50,30);
+  }
   
 
 }
-
+//function to read the foodStock
 function readStock(data) {
   foodS = data.val();
+  foodObj.updateFoodStock(foodS);
 }
 
-function writeStock(x){
+/*function writeStock(x){
   if(x <= 0) {
     x = 0;
   } else{x--}
   database.ref("/").update({Food: x});
 
   
+}*/
+//function to update the foodStock and feedTime
+function feedDog() {
+  dog.addImage(happyDogImg);
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref("/").update({
+    Food: foodObj.getFoodStock(),
+    feedTime: hour()
+  })
 }
 
-function keyPressed() {
-  if(keyCode === UP_ARROW) {
-    writeStock(foodS);
-    dog.addImage(happyDogImg);
-  }
+//function to add food
+function addFood() {
+  foodS++;
+  database.ref("/").update({
+    Food: foodS
+  })
 }
 
